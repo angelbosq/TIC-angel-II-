b995587f2c627e426cfa472d8e90f109878a5f7f
angelbosq <71437915+angelbosq@users.noreply.github.com>
2021-02-22 19:07:41 +0100

    Add files via upload

---
+++ Lista_punteros.cpp
@@ -0,0 +1,16 @@
+#include<stdio>
+#include<stdlib.h>
+#include<string.h>
+//memory alocation
+int main(){
+	char *lista[3]];
+	int cont;
+	char aux[20];
+	int longitud;
+	for(cont=0;cont<3;cont++){
+		printf("palabra %d: ");
+		scanf("%s",aux);
+		longitud=strlen(aux);
+		(char *)malloc(lomgitud);
+	}
+}
