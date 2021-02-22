e9fa982896b9321bec6d613b37446e9450caebf7
angelbosq <71437915+angelbosq@users.noreply.github.com>
2021-02-22 19:06:36 +0100

    Add files via upload

---
+++ Punteros_1.cpp
@@ -0,0 +1,11 @@
+//punteros 1
+#include<stdio.h>
+int main(){
+	char letra;
+	letra='y';
+	printf("la variable letra= %c",letra);
+	
+	return 0;
+	
+	
+}
---
+++ Punteros_2.cpp
@@ -0,0 +1,12 @@
+//punteros 2
+#include<stdio.h>
+int main(){
+	char letra;
+	letra='y';
+	printf("la variable letra= %c",letra);
+	printf("\nla posicion de memoria de letra = %X",&letra);
+	
+	return 0;
+	
+	
+}
